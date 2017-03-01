caba1472f80a42558a4b944c4d4e9bf4bc22c236
Mike Deiters <miked49er@gmail.com>
2017-03-01 09:35:42 -0500

    Added excersice files for new module

---
+++ Ex_Files_DesAesth_Web/CH04/04_08_Challenge_HTML/js/usethis.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#typeovervideo button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
